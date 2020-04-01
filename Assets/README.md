89981e0fdaaa728361ac0449bfc1812e9e20b11d
CrisTerry <crisdc88@gmail.com>
2020-03-31 22:54:34 -0700

    add Ajax call for weather and forecast

--- Assets/README.md
+++ Assets/README.md
@@ -19,7 +19,15 @@ GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
 WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+THEN I am presented with the city name, the date,
+
+an icon representation of weather conditions, weather array[0]: id 803 main:"clouds"
+
+the temperature, => main temp
+the humidity, => main humidity
+the wind speed,  => wind
+and the UV index
+
 WHEN I view the UV index
 THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 WHEN I view future weather conditions for that city
--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,119 @@
 $(document).ready(function () {
 
-    var apiKey = "9bd3bf6fe4a1fb99bfc30f6487a298d3";
-    //  city = "Orlando, Florida"
-    var city = "Orlando, Florida";
-    // var city = "San Francisco"
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
+    // create a form and ask for the city name
 
-        url: queryURL,
-        method: "GET"
-    }).
-        then(function (response) {
+    
 
+    $("#getWeather").click(function (event) {
 
-            console.log(response);
-        });
+        event.preventDefault();
+
+        var myCity = $("#searchInfo").val();
+        console.log(myCity);
+        ajaxCalls(myCity);
+
+    })
 
 
 
+    function ajaxCalls(city){
 
+        var apiKey = "9bd3bf6fe4a1fb99bfc30f6487a298d3";
+        //  city = "Orlando, Florida"
+
+        // var city = "San Francisco"
+        // Here we are building the URL we need to query the database
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+        var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+
+
+        // the temperature, => main temp
+        // the humidity, => main humidity
+        // the wind speed,  => wind
+        // and the UV index  >> change colors  Lat and long
+        var myTemperature;
+        var myHumidity;
+        var myWindSpeed;
+        var myUvIndex;
+        var myIcon;
+        // http://openweathermap.org/img/wn/01d@2x.png
+        var myLat;
+        var myLon;
+        
+        // Here we run our AJAX call to the OpenWeatherMap API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).
+            then(function (response) {
+                console.log(response);
+
+                myTemperature = response.main.temp;
+                console.log(myTemperature)
+
+                myHumidity = response.main.humidity;
+                console.log(myHumidity)
+
+                myWindSpeed = response.wind.speed;
+                console.log(myWindSpeed)
+
+                myLat = response.coord.lat;
+                console.log(myLat)
+
+                myLon = response.coord.lon;
+                console.log(myLon)
+                 
+                myIcon = response.weather[0].icon;
+                console.log(myIcon);
+                
+                var queryURLUV = "http://api.openweathermap.org/data/2.5/uvi?appid="+apiKey+"&lat="+myLat+"&lon="+myLon;
+
+
+                $.ajax({
+                    url: queryURLUV,
+                    method: "GET"
+                }).then(function(uvResponse){
+                    console.log(uvResponse)
+                    myUvIndex = uvResponse.value;
+                })
+
+
+
+
+
+            });
+
+
+
+            $.ajax({
+                url: queryURLForecast,
+                method: "GET"
+
+            }).then(function(data){
+
+                // console.log(data)
+               
+                for(var i=0 ; i< data.list.length ; i++){
+                    //var myHour = data.list[i].dt_txt;
+
+
+                    if(data.list[i].dt_txt.indexOf("12:00:00")!== -1){
+                        console.log(data.list[i]);
+
+                        // create the structure dynamycaly 
+                    }
+
+                }
+
+            })
+
+    }
+
+
+
+    
 
 
 });
